1264dadd8a72272ff1dfcacf25addb28a06ca764
Thomas Almeida <tom.tom.pop13@gmail.com>
2019-06-11 15:42:03 -0600

    Todo list complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
+import Todos from "./components/Todos"
+import Header from "./components/layout/Header"
+import AddTodo from "./components/AddTodo"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "take out the trash",
+        completed: false,
+      },
+      {
+        id: 2,
+        title: "kiss adam ",
+        completed: false,
+      },
+      {
+        id: 3,
+        title: "Learn React",
+        completed: false, //oof
+      },
+    ],
+  }
+
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      }),
+    })
+  }
+
+  delTodo = id => {
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)],
+    })
+  }
+
+  addTodo = title => {
+    const newTodo = {
+      id: this.state.todos.length + 1,
+      title,
+      completed: false,
+    }
+    this.setState({ todos: [...this.state.todos, newTodo] })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <AddTodo addTodo={this.addTodo} />
+        <Todos
+          todos={this.state.todos}
+          markComplete={this.markComplete}
+          delTodo={this.delTodo}
+        />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
